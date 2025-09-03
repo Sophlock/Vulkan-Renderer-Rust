43d632afda87f70b8e7ae31e40c6aa3c08a84eb5
Sophlock <thomas.suckfuell@t-online.de>
2025-09-03 13:32:10 +0200

    Small update do shader objects

--- src/application/renderer/rhi_assets/vulkan_material.rs
+++ src/application/renderer/rhi_assets/vulkan_material.rs
@@ -2,18 +2,23 @@ use crate::{
     application::renderer::shader_object::{ShaderObject, ShaderObjectLayout},
     application::renderer::shaders::SlangCompiler
 };
-use shader_slang::{ComponentType, Downcast};
+use shader_slang::{Blob, ComponentType, Downcast};
 use std::{
     ops::Deref,
     sync::Arc
 };
+use shader_slang::reflection::Shader;
 use vulkano::{
     device::Device,
     render_pass::RenderPass,
     shader::spirv::bytes_to_words
 };
 
-struct VulkanMaterial {}
+struct VulkanMaterial {
+    shader_object: ShaderObject,
+    vert_spirv: Blob,
+    frag_spirv: Blob,
+}
 
 impl VulkanMaterial {
     pub fn new(compiler: &SlangCompiler, device: &Arc<Device>, render_pass: Arc<RenderPass>) -> shader_slang::Result<Self> {
@@ -23,12 +28,16 @@ impl VulkanMaterial {
         let composed = Self::append_raster_entry_points(module_component, compiler)?;
         let linked = composed.link()?;
         let vert_spirv = linked.entry_point_code(0, 0)?;
-        let frag_spriv = linked.entry_point_code(1, 0)?;
+        let frag_spirv = linked.entry_point_code(1, 0)?;
         let shader_object_layout = ShaderObjectLayout::new(device);
 
-        ShaderObject::new(device, render_pass, shader_object_layout, bytes_to_words(vert_spirv.as_slice()).unwrap().deref(), bytes_to_words(frag_spriv.as_slice()).unwrap().deref());
+        let shader_object = ShaderObject::new(device, render_pass, shader_object_layout, bytes_to_words(vert_spirv.as_slice()).unwrap().deref(), bytes_to_words(frag_spirv.as_slice()).unwrap().deref());
 
-        Ok(Self {})
+        Ok(Self {
+            shader_object,
+            vert_spirv,
+            frag_spirv,
+        })
     }
 
     fn append_raster_entry_points(
