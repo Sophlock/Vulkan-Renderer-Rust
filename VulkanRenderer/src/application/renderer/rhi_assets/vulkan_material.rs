7ad222a9ec7e71e4a177addb42e476b1f09617f0
Sophlock <38535540+Thomipon@users.noreply.github.com>
2026-01-22 16:19:19 +0100

    Use custom slang binding fork

--- VulkanRenderer/src/application/renderer/rhi_assets/vulkan_material.rs
+++ VulkanRenderer/src/application/renderer/rhi_assets/vulkan_material.rs
@@ -4,7 +4,7 @@ use crate::{
     application::renderer::shader_object::{ShaderObject, ShaderObjectLayout},
     application::renderer::shaders::SlangCompiler,
 };
-use shader_slang::{Blob, ComponentType, Downcast, Error, IUnknown};
+use shader_slang::{Blob, ComponentType, Error, IUnknown};
 use std::{ops::Deref, sync::Arc};
 use vulkano::{device::Device, render_pass::RenderPass, shader::spirv::bytes_to_words};
 
@@ -23,13 +23,13 @@ impl VulkanMaterial {
         material_name: &str,
     ) -> shader_slang::Result<Self> {
         let module = compiler.session().load_module(module_name)?;
-        let module_component = module.downcast();
+        let module_component: ComponentType = module.into();
         // TODO: Specialize
         let material_reflection = module_component
             .layout(0)?
             .find_type_by_name(material_name)
             .unwrap();
-        let composed = Self::append_raster_entry_points(module_component, compiler)?;
+        let composed = Self::append_raster_entry_points(&module_component, compiler)?;
         let linked = composed.link()?;
         let vert_spirv = linked.entry_point_code(0, 0)?;
         let frag_spirv = linked.entry_point_code(1, 0)?;
@@ -63,9 +63,9 @@ impl VulkanMaterial {
             .unwrap();
         compiler.session().create_composite_component_type(&[
             component.clone(),
-            raster_module.downcast().clone(),
-            vertex_main.downcast().clone(),
-            fragment_main.downcast().clone(),
+            raster_module.into(),
+            vertex_main.into(),
+            fragment_main.into(),
         ])
     }
 }
