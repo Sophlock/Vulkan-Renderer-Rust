d61f967918d4969ab581514a305440ccf469556e
Sophlock <38535540+Thomipon@users.noreply.github.com>
2026-02-05 14:25:25 +0100

    Emit spirv code indirectly

--- VulkanRenderer/src/application/rhi/shaders.rs
+++ VulkanRenderer/src/application/rhi/shaders.rs
@@ -12,13 +12,15 @@ pub struct SlangCompiler {
 impl SlangCompiler {
     pub fn new(shader_base_path: &Path) -> Self {
         let global_session = GlobalSession::new().unwrap();
+        println!("Using slang compiler version {}", global_session.build_tag_string());
         let targets = [TargetDesc::default()
             .format(CompileTarget::Spirv)
-            .profile(global_session.find_profile("spirv_1_5"))];
+            .profile(global_session.find_profile("spirv_1_6"))];
         let search_paths = [shader_base_path.to_str().unwrap().as_ptr() as *const i8];
         let options = CompilerOptions::default()
             .optimization(OptimizationLevel::High)
-            .emit_spirv_directly(true)
+            // TODO: Once Vulkano supports the vulkan extension for compute shader derivatives (or slang doesn't use it anymore), we should directly emit spirv code
+            .emit_spirv_directly(false)
             .matrix_layout_column(true);
         let session_description = SessionDesc::default()
             .targets(&targets)
