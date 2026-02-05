68346e14b84ac21c72c81b8134db79b05fefc504
Sophlock <38535540+Thomipon@users.noreply.github.com>
2026-02-05 14:17:05 +0100

    Update vertex names to more modern slang

--- VulkanRenderer/src/application/assets/asset_traits.rs
+++ VulkanRenderer/src/application/assets/asset_traits.rs
@@ -15,16 +15,16 @@ use crate::application::{
 #[derive(BufferContents, Copy, Clone, vertex_input::Vertex)]
 #[repr(C)]
 pub struct Vertex {
-    #[name("input.position")]
+    #[name("input_position_0")]
     #[format(R32G32B32_SFLOAT)]
     pub position: [f32; 3],
-    #[name("input.normal")]
+    #[name("input_normal_0")]
     #[format(R32G32B32_SFLOAT)]
     pub normal: [f32; 3],
-    #[name("input.tangent")]
+    #[name("input_tangent_0")]
     #[format(R32G32B32_SFLOAT)]
     pub tangent: [f32; 3],
-    #[name("input.textureCoordinate")]
+    #[name("input_textureCoordinate_0")]
     #[format(R32G32_SFLOAT)]
     pub texture_coordinates: [f32; 2],
 }
